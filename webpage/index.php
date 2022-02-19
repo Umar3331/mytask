4f8375add7f8bad3702b7e13cc26badf0dd38215
Janis <janis@lapuvieta.lv>
2022-02-19 13:02:36 +0200

    MySQL connection sensitive data moved from index.php to .env file for security reasons.

--- webpage/index.php
+++ webpage/index.php
@@ -1,12 +1,20 @@
 <?php
+/**
+ * Load confidential information from variables file like .env,
+ * storing mysql connection sensitive information in public codebase is not
+ * recommended.
+ */
+require_once('DotEnv.php');
+(new DevCoder\DotEnv(__DIR__ . '/.env'))->load();
+
 $username = filter_input(INPUT_POST, 'username');
 $password = filter_input(INPUT_POST, 'password');
 if (!empty($username)){
 if (!empty($password)){
-$host = "localhost";
-$dbusername = "DBname";
-$dbpassword = "DBpassword";
-$dbname = "youtube";
+$host = getenv('DB_HOST');
+$dbusername = getenv('DB_USERNAME');
+$dbpassword = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
 $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
 
 if (mysqli_connect_error()){
